6eaf1e7dcbf92c8c322545a8f491e30b1b36b0b0
Evgenii Yakobenko <dillas90@gmail.com>
2018-12-11 19:52:53 +0300

    Added authentication

--- packages/server/src/resolvers/message.js
+++ packages/server/src/resolvers/message.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-return-await */
 export default {
   Query: {
-    messages: async (parent, args, { models }) => {
-      return await models.Message.findAll()
+    messages: (parent, args, { models }) => {
+      return models.Message.findAll()
     },
     message: async (parent, { id }, { models }) => {
       return await models.Message.findById(id)
@@ -11,10 +11,14 @@ export default {
 
   Mutation: {
     createMessage: async (parent, { text }, { me, models }) => {
-      return await models.Message.create({
-        text,
-        userId: me.id
-      })
+      try {
+        return await models.Message.create({
+          text,
+          userId: me.id
+        })
+      } catch (error) {
+        throw new Error(error)
+      }
     },
     deleteMessage: async (parent, { id }, { models }) => {
       return await models.Message.destroy({ where: { id } })
