d66c09b000a0dc6747ce380cebbede66af223384
Evgenii Yakobenko <dillas90@gmail.com>
2018-12-14 12:38:05 +0300

    Added message pagination example

--- packages/server/src/schema/message.js
+++ packages/server/src/schema/message.js
@@ -3,12 +3,22 @@ import { gql } from 'apollo-server'
 export default gql`
     extend type Query {
         message(id: ID!): Message!
-        messages: [Message!]!
+        messages(cursor: String, limit: Int): MessageConnection!
     }
 
     extend type Mutation {
         createMessage(text: String!): Message!
         deleteMessage(id: ID!): Boolean!
+    }
+
+    type MessageConnection {
+        edges: [Message!]!
+        pageInfo: PageInfo!
+    }
+
+    type PageInfo {
+        hasNextPage: Boolean!
+        endCursor: String!
     }
     
     type Message {
